6e497c200f2710df5cd2aca8ef7947c18612c9b9
Waggler <khdavis4@gmail.com>
2021-01-21 20:01:58 -0500

    Mark1
    Added Player Movement, Enemy Movement, Pickups, and UI

---
+++ BadDogg2/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Vector3 movement = new Vector3(0,0,0);
+
+public class Enemy : MonoBehaviour
+{
+    Vector3 movement = new Vector3(0, 0, 0);
+    public GameObject[] waypoints;
+    //public Vector3 currentPosition;
+    public GameObject player;
+    //public GameObject selfRef;
+    [SerializeField] private float speed;
+
+    void Start()
+    {
+        CallCorotuine();
+
+    }
+
+
+    public void SetDestination()
+    {
+        for(int i = 0; i < waypoints.Length; i++)
+        {
+   
+        }
+        
+
+        //
+        //array of waypoints represent where we will go.  
+        //length of the array is how many times we will move.  Its our destination
+        // if we reach the end of the array we will reset and go back to 0 (Our first destination)
+        //once this works 
+        // if(currentPosition < 1f == true && player.isTouchingPlayer == true)
+        // kill player
+        //else keepMoving()
+    }
+
+    public void CallCorotuine()
+    {
+        StartCoroutine(IStartmovement());
+    }
+
+
+    public IEnumerator IStartmovement()
+    {
+        yield return new WaitForSeconds(2f);
+    }
+
+
+    private void MoveToPlayer()
+    {
+        float step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+
+        // Check if the position of the cube and sphere are approximately equal.
+        //You can put logic here for when Enemy is close to player
+        if (Vector3.Distance(transform.position, player.transform.position) < .1f)
+        {
+
+        }
+    }
+
+
+    void Update()
+    {
+        MoveToPlayer();
+    }
+}
---
+++ BadDogg2/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerScript : MonoBehaviour
+{
+    public Text countText; //create text for pickups
+    public Text winText; //create win text
+    public Text score;
+    public float speed;
+
+    private Rigidbody2D rd2d;
+    private int count; //set count to use integers
+    private int lives;
+    private int scoreValue = 0;
+    private float hozMovement;
+    private float vertMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!this.gameObject.activeInHierarchy)
+        {
+            this.gameObject.SetActive(true);
+        }
+
+        rd2d = GetComponent<Rigidbody2D>();
+        count = 0; //set count to 0
+        lives = 1; //set lives to 3
+        winText.text = "";
+        SetCountText(); 
+        SetLivesText();
+        hozMovement = Input.GetAxis("Horizontal");
+        vertMovement = Input.GetAxis("Vertical");
+    }
+
+
+    private void Update()
+    {
+        Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        this.transform.position += movementDirection * speed * Time.deltaTime;
+    } // END Update
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp")) //when a pickup is collected, add one to count and display it in count text
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy")) //when an enemy is collected, subtract a life and display it in lives text
+        {
+            other.gameObject.SetActive(false);
+            lives -= 1;
+            SetLivesText();
+        }
+    } // END OnTriggerEnter2D
+
+
+    private void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+
+        if (count >= 5) //if count is 20 or higher display win text
+        {
+            winText.text = "You Win! Game created by Kevin H. Davis!";
+        }
+    } // END SetCountText
+
+
+    private void SetLivesText()
+    {
+        if (lives <= 0) //if lives are 0 or less, destroy player and display text
+        {
+            winText.text = "You Lose! Please Try Again!";
+            this.gameObject.SetActive(false);
+            //Destroy(this.gameObject);
+        }
+    } // END SetLivesText
+}
